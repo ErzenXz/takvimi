1cb3e60f5ec27406414dc12f4479bfe8ec83bcdb
ErzenXs <njnana2017@gmail.com>
2022-11-13 01:36:17 +0100

    UI changes

--- second.js
+++ second.js
@@ -1,6 +1,8 @@
 
 let latitude, longitude, country;
 
+const PROXY = "https://erproxy.herokuapp.com/";
+
 getLocation();
 
 function getLocation() {
@@ -44,6 +46,15 @@ function getPrayerTimes(){
         method: 3
       });
 
+      var hadithUrl = PROXY + "https://onehadith.org";
+      var hadithfUrl = hadithUrl + "/api/random";
+      var hadith_url = geturl(hadithfUrl, { lan: "al"});
+      fetch(hadith_url).then((response) => response.json()).then((data) => {
+        console.log(data);
+        document.getElementById("hadith").innerHTML = data.hadith + "<br>" + data.grade;
+      });
+
+
       let url = `https://onehadith.org/api/random?lan=en&country=${country}&timestamp=${timestamp}`;
         
         fetch(url)
@@ -54,15 +65,28 @@ function getPrayerTimes(){
                 .then((data2) => {
                     if(dtm == false){
 
-                        document.getElementById("imsaku").innerText = "Imsak:" + data2.data.timings.Imsak;
-                        document.getElementById("lindjaDiellit").innerText = "Sunrise: " + data2.data.timings.Sunrise;
-                        document.getElementById("dreka").innerText = "Dhuhr: " + data2.data.timings.Dhuhr;
-                        document.getElementById("ikindia").innerText = "Asr: " + data2.data.timings.Asr;
-                        document.getElementById("akshami").innerText = "Maghrib: " + data2.data.timings.Maghrib;
-                        document.getElementById("jacia").innerText = "Isha: " + data2.data.timings.Isha;
-                        document.getElementById("sabahu").innerText = "Fajr: " + data2.data.timings.Fajr;
+                        document.getElementById("imsakuN").innerText = "Imsak:";
+                        document.getElementById("lindjaDiellitN").innerText = "Sunrise: ";
+                        document.getElementById("drekaN").innerText = "Dhuhr: ";
+                        document.getElementById("ikindiaN").innerText = "Asr: ";
+                        document.getElementById("akshamiN").innerText = "Maghrib: ";
+                        document.getElementById("jaciaN").innerText = "Isha: ";
+                        document.getElementById("sabahuN").innerText = "Fajr: ";
+                        document.getElementById("imsakuT").innerText = data2.data.timings.Imsak;
+                        const myArray = String(data2.data.timings.Sunrise).split(":");
+                        if(Number(myArray[0]) < 10){                      
+                          document.getElementById("lindjaDiellitT").innerText = "0" + data2.data.timings.Sunrise;
+                        } else {
+                          document.getElementById("lindjaDiellitT").innerText = data2.data.timings.Sunrise;
+                        }
+                        document.getElementById("drekaT").innerText = data2.data.timings.Dhuhr;
+                        document.getElementById("ikindiaT").innerText =data2.data.timings.Asr;
+                        document.getElementById("akshamiT").innerText = data2.data.timings.Maghrib;
+                        document.getElementById("jaciaT").innerText = data2.data.timings.Isha;
+                        document.getElementById("sabahuT").innerText = data2.data.timings.Fajr;
                     }
-                    document.getElementById("sabahu").innerText = "Sabahu: " + data2.data.timings.Fajr;
+                    document.getElementById("sabahuN").innerText = "Sabahu: ";
+                    document.getElementById("sabahuT").innerText = data2.data.timings.Fajr;
                     document.getElementById("hijri").innerText = "Hijri: " + data2.data.date.hijri.date;
                     document.getElementById("weekday").innerText = "Weekday: " + data2.data.date.gregorian.weekday.en;
                     document.getElementById("holiday").innerText = "Holiday: " + (data2.data.date.hijri.holidays[0] ?? "No holiday");
@@ -75,22 +99,47 @@ function getPrayerTimes(){
                 });
 
             if(dtm != false) {
-                document.getElementById("imsaku").innerText = "Imsaku: " +  data.timings.imsak;
-                document.getElementById("lindjaDiellit").innerText = "Lindja e diellit: " + data.timings.sunrise;
-                document.getElementById("dreka").innerText = "Dreka: " + data.timings.dhuhr;
-                document.getElementById("ikindia").innerText = "Ikindia: " + data.timings.asr;
-                document.getElementById("akshami").innerText = "Akshami: " + data.timings.maghrib;
-                document.getElementById("jacia").innerText = "Jacia: " + data.timings.isha;
+                document.getElementById("imsakuN").innerText = "Imsaku: ";
+                document.getElementById("lindjaDiellitN").innerText = "Lindja e diellit: ";
+                document.getElementById("drekaN").innerText = "Dreka: ";
+                document.getElementById("ikindiaN").innerText = "Ikindia: ";
+                document.getElementById("akshamiN").innerText = "Akshami: ";
+                document.getElementById("jaciaN").innerText = "Jacia: ";
+                document.getElementById("imsakuT").innerText = data.timings.imsak;
+                const myArray = String(data.timings.sunrise).split(":");
+                if(Number(myArray[0]) < 10){                      
+                  document.getElementById("lindjaDiellitT").innerText = "0" + data.timings.sunrise;
+                } else {
+                  document.getElementById("lindjaDiellitT").innerText = data.timings.sunrise;
+                }
+                document.getElementById("drekaT").innerText = data.timings.dhuhr;
+                document.getElementById("ikindiaT").innerText = data.timings.asr;
+                document.getElementById("akshamiT").innerText = data.timings.maghrib;
+                document.getElementById("jaciaT").innerText = data.timings.isha;
                 document.getElementById("georgian").innerText = "Gregorian: " + data.timings.day + "-" + data.timings.month + "-" + new Date().getFullYear();
-            }
-            
+              }
+            document.getElementById("loading").classList.add("hidden");
+            document.getElementById("main").classList.remove("hidden");
           })
           .catch((error) => {
-            console.error("Error:", error);
+            document.getElementById("loading").innerText = "An error occurred\n\n" + error.message;
           });
-      
-          
 
+
+          let date = new Date();
+          let month = date.getMonth() + 1;
+          let day = date.getDate();
+          let year = date.getFullYear();
+          let hour = date.getHours();
+          let minute = date.getMinutes();
+          let seconds = date.getSeconds();
+
+          if(hour < 10){
+            hour = "0" + hour;
+          }
+          if(seconds < 10){
+            seconds = "0" + seconds;
+          }
       }
 
 
@@ -201,3 +250,36 @@ function checkTimes(){
 setInterval(() => {
   checkTimes();
 }, 60000);
+
+var getFirstBrowserLanguage = function () {
+  var nav = window.navigator,
+  browserLanguagePropertyKeys = ['language', 'browserLanguage', 'systemLanguage', 'userLanguage'],
+  i,
+  language;
+
+  // support for HTML 5.1 "navigator.languages"
+  if (Array.isArray(nav.languages)) {
+    for (i = 0; i < nav.languages.length; i++) {
+      language = nav.languages[i];
+      if (language && language.length) {
+        return language;
+      }
+    }
+  }
+
+  // support for other well known properties in browsers
+  for (i = 0; i < browserLanguagePropertyKeys.length; i++) {
+    language = nav[browserLanguagePropertyKeys[i]];
+    if (language && language.length) {
+      return language;
+    }
+  }
+
+  return null;
+};
+
+function refreshData(){
+  document.getElementById("loading").classList.remove("hidden");
+  document.getElementById("main").classList.add("hidden");
+  getPrayerTimes();
+}
